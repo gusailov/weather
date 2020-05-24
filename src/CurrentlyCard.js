188bbef952bc6f63e1b471d87e82dc75e1e8d472
Gusailov <gusailov@gmail.com>
2020-05-24 22:09:00 +0300

    card

--- src/CurrentlyCard.js
+++ src/CurrentlyCard.js
@@ -2,25 +2,27 @@ import React, { useState, useEffect } from "react";
 import forecast from "./forecast.json";
 import Skycons from "react-skycons";
 
-function myFunction() {
-  const regex = /-/gi;
-  var str = "partly-cloudy-night";
-  var res = str.toUpperCase().replace(regex, "_");
-  return res;
-}
-
-console.log(myFunction());
-
-console.log(forecast.currently.summary);
+const iconName = forecast.currently.icon.toUpperCase().replace(/-/gi, "_");
 const currently = forecast.currently;
+console.log(currently.summary);
+
 function CurrentlyCard() {
   return (
     <div>
       <div className="card-deck">
         {
-          <div className="card">
+          <div
+            className="card"
+            style={{
+              width: "18" + "rem",
+              height: "40px",
+              paddingLeft: "16px",
+              marginTop: "2px",
+              marginBottom: "500px",
+            }}
+          >
             <div className="card-img-top">
-              <Skycons color="black" icon="PARTLY_CLOUDY_DAY" autoplay={true} />
+              <Skycons color="black" icon={iconName} autoplay={true} />
             </div>
             <div className="card-body">
               <h5 className="card-title">{currently.summary}</h5>
