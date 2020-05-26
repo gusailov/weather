b67f212d0c4c1429e5008a1482f89670b0d8b3d7
Gusailov <gusailov@gmail.com>
2020-05-26 23:59:53 +0300

    daily_Carousel

--- src/DailyCards.js
+++ src/DailyCards.js
@@ -2,22 +2,38 @@ import React, { useState, useEffect } from "react";
 import forecast from "./forecast.json";
 import Skycons from "react-skycons";
 import dateFormat from "./serviceWorker";
+import { Carousel } from "react-materialize";
 
 //const iconName = forecast.daily.icon.toUpperCase().replace(/-/gi, "_");
-const daily = forecast.daily;
-console.log(dailys);
+const daily = forecast.daily.data;
+console.log(daily);
 
 const date = dateFormat(daily.time);
 
 function DailyCards() {
   return (
-    <ul>
-      {daily.map((item) => (
-        <li key={item.name}>
-          {item.name} {item.price}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {" "}
+        <Carousel
+          carouselId="Carousel-2"
+          className="white-text center"
+          options={{
+            fullWidth: true,
+            indicators: true,
+          }}
+        >
+          {daily.map((item) => (
+            <li key={item.time}>
+              <div className="red">
+                <h2>{dateFormat(item.time)}</h2>
+                <p>{item.summary}</p>
+              </div>
+            </li>
+          ))}
+        </Carousel>
+      </ul>
+    </div>
   );
 }
 
