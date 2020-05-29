25ac13337093eede29352e96561937d9d52a6345
gusailov <gusailov@gmail.com>
2020-05-29 12:02:21 +0300

    styling

--- src/CurrentlyCard.js
+++ src/CurrentlyCard.js
@@ -3,40 +3,46 @@ import forecast from "./forecast.json";
 import { dateFormat } from "./utils";
 import sprite from './icons/sprite.svg'
 
-
 const currentlyIconName = forecast.currently.icon;
-
 const currently = forecast.currently;
 const date = dateFormat(currently.time);
 
 function CurrentlyCard() {
       return (
         <div className="container">
-      
-      
-        <div className="col s12 m6">
-          <div className="card">
-            <span className="card-title blue-text text-darken-2">{date}</span>
-            <div className="card-image">
-            <svg className="card-image">
-	<use xlinkHref={sprite+"#"+currentlyIconName}></use>
-</svg>     
-              <span className="card-title blue-text text-darken-2">
+            <div className="card row">
+              <div className="col s12">
+            <div className="card-title blue-text flow-text row center-align">
+              <div>
+              {date} {Math.round(currently.temperature) }<span>&deg;C</span>
+              </div>
+              </div>
+            
+            <div className="row">
+            <svg className="card-image">	<use xlinkHref={sprite+"#"+currentlyIconName}></use></svg>     </div>
+              <div className="card-title blue-text text-darken-2 center-align col">
                 {currently.summary}
-              </span>
-              <div className="btn-floating halfway-fab waves-effect waves-light red">
+              </div>
+              
+            </div>
+            
+              
+              
+            <div className="card-content col s12">
+              <div className="col s2">
+                {'WindSpeed -'+ Math.round(currently.windSpeed) + 'm/s'}
+              </div>
+              <div className="col s2">
+                {'Feels like -'+ Math.round(currently.apparentTemperature)}<span>&deg;C</span>
+              </div>
+              <div className="col s2">
+              <div className="btn-floating waves-effect waves-light red">
                 <i className="material-icons">add</i>
               </div>
+              </div>
             </div>
-            <div className="card-content">
-              <p>
-                I am a very simple card. I am good at containing small bits of
-                information. I am convenient because I require little markup to
-                use effectively.
-              </p>
-            </div>
+          
           </div>
-        </div>
       
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-//import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 //import "semantic-ui-css/semantic.min.css";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
