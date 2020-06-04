9d7de3d77de5ad362fb1878fea03839f6a583494
Gusailov <gusailov@gmail.com>
2020-06-04 23:46:39 +0300

    location_search_classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,88 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CurrentlyCard from "./CurrentlyCard";
 import DailyCards from "./DailyCards";
 import HourlyCards from "./HourlyCards";
-//import forecast from "../forecastOpen.json";
-import Api from "./Api";
+//import SearchPlace from "./SearchPlace";
+import SearchGoo from "./Search";
 
 function App() {
   const [error, setError] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  
-  const searchName = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=46.657685&lon=31.015853&units=metric&exclude={part}&appid=`;
+  const [position, setPosition] = useState([]);
+
+  useEffect(() => {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    function success(pos) {
+      var crd = pos.coords;
+      setPosition(crd);
+    }
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  //const searchPosition = (pos) => {
+  //   setPosition(pos);
+  // };
+  console.log(position);
+
+  const getForecast = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${position.latitude}&lon=${position.longitude}&units=metric&exclude={part}&appid=`;
     await fetch(url)
       .then((res) => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
           setItems(result);
-                },
-        (message) => {
+        },
+        (error) => {
+          console.log("message", error);
           setIsLoaded(true);
-          setError(message);
+          setError(error);
         }
       );
   };
-  useEffect(() => {    
-    searchName()
-  }, []);
-  
-  console.log("items- ", items);
-  console.log("isLoaded-" ,isLoaded);
-  console.log("error-" , error);
-  
-  if (error) {
-    return <div>Помилка: {error}</div>;
+  useEffect(() => {
+    getForecast();
+  }, [position]);
+
+  if (items.message) {
+    return (
+      <div>
+        <div></div>
+        Помилка: {items.message}
+      </div>
+    );
   } else if (!isLoaded || items.length === 0) {
     return <div>Завантаження...</div>;
   } else {
-  return (
-        <div>
-            <CurrentlyCard forecast={items}
-        isLoaded={isLoaded}
-        error={error}></CurrentlyCard>
-        <DailyCards forecast={items}
-        isLoaded={isLoaded}
-        error={error}></DailyCards>
-        <HourlyCards forecast={items}
-        isLoaded={isLoaded}
-        error={error}></HourlyCards>
-          </div>
-  );
-}}
+    return (
+      <div>
+        <SearchGoo className="row"></SearchGoo>
+        <CurrentlyCard
+          forecast={items}
+          isLoaded={isLoaded}
+          error={error}
+        ></CurrentlyCard>
+        <DailyCards
+          forecast={items}
+          isLoaded={isLoaded}
+          error={error}
+        ></DailyCards>
+        <HourlyCards
+          forecast={items}
+          isLoaded={isLoaded}
+          error={error}
+        ></HourlyCards>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GetCoords.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function GetCoords() {
+  const [position, setPosition] = useState([]);
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  function success(pos) {
+    var crd = pos.coords;
+    setPosition(crd);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+  return {
+    position,
+  };
+}
+
+export default GetCoords;
