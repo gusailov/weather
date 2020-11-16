5b4b64fc0a1754fe09ed0f77fb4e7203c78adf77
Gusailov <gusailov@gmail.com>
2020-11-16 21:31:40 +0200

    styling

--- src/components/DailyCards.jsx
+++ src/components/DailyCards.jsx
@@ -6,6 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import HourlyCards from './HourlyCards';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -77,6 +78,7 @@ export default function ScrollableTabsButtonAuto() {
           <Tab label="Item Seven" {...a11yProps(6)} />
         </Tabs>
       </AppBar>
+      <HourlyCards value={value} index={0}></HourlyCards>
       <TabPanel value={value} index={0}>
         Item One
       </TabPanel>
--- src/components/HourlyCards.jsx
+++ src/components/HourlyCards.jsx
@@ -4,54 +4,12 @@ import Slider from "react-slick";
 import { SampleNextArrow, SamplePrevArrow } from "./Arrows";
 
 function HourlyCards(props) {
-  const { forecast } = props;
-  const hourly = forecast.hourly;
-
-  const day = new Date(props.active * 1000);
-
-  const filter = hourly.filter(
-    (item) => new Date(item.dt * 1000).getDate() === day.getDate()
-  );
-
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-  };
+  console.log(props)
 
   return (
     <div className="">
       <p className="card-title">Hourly Forecast</p>
-      <Slider {...settings}>
-        {filter.map((item) => (
-          <div key={item.dt} className="col sm">
-            <div className="tab-content">
-              <img
-                className="card-img-top"
-                src={
-                  "http://openweathermap.org/img/wn/" +
-                  item.weather[0].icon +
-                  "@2x.png"
-                }
-                alt="альтернативный текст"
-              />
-              <span className="card-text">
-                <p>{Math.round(item.temp) + "℃"}</p>
-              </span>
-              <div className="card-text mx-auto">
-                <p>{item.summary}</p>
-              </div>
-              <div className="card-text mx-auto">
-                <p>{dateFormatHourly(item.dt, props.lang)}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </Slider>
+
     </div>
   );
 }
