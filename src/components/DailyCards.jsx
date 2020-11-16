97f78a8e353c1089cdc65d4c7227ee446e84c171
Gusailov <gusailov@gmail.com>
2020-11-16 22:24:58 +0200

    maping

--- src/components/DailyCards.jsx
+++ src/components/DailyCards.jsx
@@ -7,6 +7,7 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import HourlyCards from './HourlyCards';
+import { dateFormat } from "../utils";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -49,16 +50,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ScrollableTabsButtonAuto() {
+export default function ScrollableTabsButtonAuto(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-
+  const { forecast } = props;
+  const daily = forecast.daily;
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <div className={classes.root}>
+
       <AppBar position="static" color="default">
         <Tabs
           value={value}
@@ -69,37 +72,20 @@ export default function ScrollableTabsButtonAuto() {
           scrollButtons="auto"
           aria-label="scrollable auto tabs example"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
+          {daily.map((item) =>
+
+            <Tab key={item.dt} icon={dateFormat(item.dt, props.lang)} {...a11yProps(0)} />
+
+          )}
+
         </Tabs>
       </AppBar>
+      {daily.map((item) => <TabPanel value={value} index={0}>
+        <div>{item.dt}</div>
+      </TabPanel>)}
       <HourlyCards value={value} index={0}></HourlyCards>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+
+
     </div>
   );
 }
