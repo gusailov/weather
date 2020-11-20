5125404ff35a2f90c6eeb259cfdff85e8abb9e05
gusailov <gusailov@gmail.com>
2020-11-20 14:13:09 +0200

    spinner_err

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types'
 
 
 const Spinner = ({ children }) => {
-    return (
-        <div className="d-flex justify-content-center">
-        <div className="spinner-border" role="status">
-          <span className="sr-only">{children}</span>
-        </div>
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border" role="status">
+        <span className="sr-only">{children}</span>
       </div>
-    )
+    </div>
+  )
 }
 
 Spinner.propTypes = {
-    children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired
 }
 Spinner.defaultProps = {
-    children: "films loading"
+  children: "LOADING"
 }
 
 
