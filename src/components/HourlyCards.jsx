3d620fc0bcc7c4148566034db1aa8f7cad471bd5
gusailov <gusailov@gmail.com>
2020-11-27 12:07:27 +0200

    finish_slider

--- src/components/HourlyCards.jsx
+++ src/components/HourlyCards.jsx
@@ -17,72 +17,68 @@ const useStyles = makeStyles((theme) => ({
   disabled: {
     color: 'red',
   },
+  sliderrrr: {
+    display: 'flex',
+    alignItems: 'center'
+
+  }
 }));
-const settings = {
 
-  infinite: false,
-  speed: 500,
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  initialSlide: 0,
-  nextArrow: <SampleNextArrow />,
-  prevArrow: <SamplePrevArrow />,
-
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 6,
-        slidesToScroll: 1,
-
-
-      }
-    },
-    {
-      breakpoint: 830,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: false,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
-  ]
-};
 function HourlyCards(props) {
   const { forecast, lang } = props;
   const hourly = forecast.hourly;
   const classes = useStyles();
-
   const { t } = useTranslation();
-
-
-
-
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    className: classes.sliderrrr,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 830,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      }
+    ]
+  };
   return (
-
     <div className={classes.root}>
-      <Paper elevation={2} style={{ padding: '1rem' }}>
+      <Paper className="tab-content" elevation={2} style={{ padding: '1rem' }}>
         <Typography variant="button" component="p">
           {t('Hourly Forecast')}
 
         </Typography>
-
-
         <Slider {...settings}>
           {hourly.map((item) =>
             < HourlyCard lang={lang} forecast={item} key={item.dt} />)}
